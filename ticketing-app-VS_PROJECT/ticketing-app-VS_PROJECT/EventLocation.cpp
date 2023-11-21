05b0491305e13db852fc08eb0440680106f5cdfa
vladboj <vlad.bojan0@gmail.com>
2023-11-21 12:59:38 +0200

    remove LocationType attribute

--- ticketing-app-VS_PROJECT/ticketing-app-VS_PROJECT/EventLocation.cpp
+++ ticketing-app-VS_PROJECT/ticketing-app-VS_PROJECT/EventLocation.cpp
@@ -2,16 +2,14 @@
 #include <string>
 #include <iostream>
 
-EventLocation::EventLocation() : name(""), address(""), type(NONE) {}
-EventLocation::EventLocation(std::string newName, std::string newAddress, LocationType newType) {
+EventLocation::EventLocation() : name(""), address("") {}
+EventLocation::EventLocation(std::string newName, std::string newAddress) {
 	this->setName(newName);
 	this->setAddress(newAddress);
-	this->setType(newType);
 }
 EventLocation::EventLocation(const EventLocation& toBeCopied) {
 	this->name = toBeCopied.name;
 	this->address = toBeCopied.address;
-	this->type = toBeCopied.type;
 }
 
 void EventLocation::setName(std::string newName) {
@@ -26,12 +24,6 @@ void EventLocation::setAddress(std::string newAddress) {
 	}
 	this->address = newAddress;
 }
-void EventLocation::setType(LocationType newType) {
-	if (newType == NONE) {
-		throw std::exception("Invalid location type");
-	}
-	this->type = newType;
-}
 
 std::string EventLocation::getName() {
 	return this->name;
@@ -39,14 +31,10 @@ std::string EventLocation::getName() {
 std::string EventLocation::getAddress() {
 	return this->address;
 }
-LocationType EventLocation::getType() {
-	return this->type;
-}
 
 void EventLocation::operator=(const EventLocation& toBeCopied) {
 	this->name = toBeCopied.name;
 	this->address = toBeCopied.address;
-	this->type = toBeCopied.type;
 }
 
 void operator>>(std::istream& console, EventLocation& myEventLocation) {
@@ -54,40 +42,9 @@ void operator>>(std::istream& console, EventLocation& myEventLocation) {
 	std::getline(console, myEventLocation.name);
 	std::cout << "\nAddress: ";
 	std::getline(console, myEventLocation.address);
-	std::cout << "\nLocation Type:";
-	std::cout << "\n1->Theater, 2->Stadium, 3->Standing Venue\n";
-	int userInput;
-	console >> userInput;
-	switch (userInput)
-	{
-	case 1:
-		myEventLocation.type = theater;
-		break;
-	case 2:
-		myEventLocation.type = stadium;
-		break;
-	case 3:
-		myEventLocation.type = standingVenue;
-		break;
-	}
 }
 
 void operator<<(std::ostream& console, const EventLocation& myEventLocation) {
 	console << "\nName: " << myEventLocation.name;
 	console << "\nAddress: " << myEventLocation.address;
-	switch (myEventLocation.type)
-	{
-	case 1:
-		console << "\nType: Theater";
-		break;
-	case 2:
-		console << "\nType: Stadium";
-		break;
-	case 3:
-		console << "\nType: Standing Venue";
-		break;
-	default:
-		console << "\nType: NONE";
-		break;
-	}
 }
--- ticketing-app-VS_PROJECT/ticketing-app-VS_PROJECT/EventLocation.h
+++ ticketing-app-VS_PROJECT/ticketing-app-VS_PROJECT/EventLocation.h
@@ -2,24 +2,19 @@
 #include <string>
 #include <iostream>
 
-enum LocationType {NONE, theater, stadium, standingVenue};
-
 class EventLocation {
 	std::string name;
 	std::string address;
-	LocationType type;
 public:
 	EventLocation();
-	EventLocation(std::string newName, std::string newAddress, LocationType newType);
+	EventLocation(std::string newName, std::string newAddress);
 	EventLocation(const EventLocation& toBeCopied);
 
 	void setName(std::string newName);
 	void setAddress(std::string newAddress);
-	void setType(LocationType newType);
 
 	std::string getName();
 	std::string getAddress();
-	LocationType getType();
 
 	void operator=(const EventLocation& toBeCopied);
 
