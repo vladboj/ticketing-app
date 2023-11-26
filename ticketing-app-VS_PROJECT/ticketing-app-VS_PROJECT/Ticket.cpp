5cd6bc72eb8ff1e7c40f6c9fbf9b7f5f60b5f0cf
vladboj <vlad.bojan0@gmail.com>
2023-11-26 19:16:35 +0200

    overload operator[]

--- ticketing-app-VS_PROJECT/ticketing-app-VS_PROJECT/Ticket.cpp
+++ ticketing-app-VS_PROJECT/ticketing-app-VS_PROJECT/Ticket.cpp
@@ -1,6 +1,7 @@
 #include "Ticket.h"
 #include "Event.h"
 #include <iostream>
+#include <string>
 
 // local implementation of rand function to avoid using external libraries :D
 // i might have stolen it :D but now i understand how a Linear Congruential Generator works
@@ -24,6 +25,9 @@ Ticket* Ticket::tickets = nullptr;
 int Ticket::noTickets = 0;
 
 int Ticket::NEXT_TICKET_ID = my_rand::rand();
+
+const int Ticket::MIN_NAME_LENGTH = 3;
+const int Ticket::MAX_NAME_LENGTH = 50;
 
 // STATIC METHODS
 void Ticket::printTickets() {
@@ -62,10 +66,10 @@ Ticket* Ticket::getTickets() {
 }
 
 // DEFAULT CONSTRUCTOR
-Ticket::Ticket() : id(NEXT_TICKET_ID), associatedEvent(Event::Event()), vipStatus(false) {}
+Ticket::Ticket() : id(NEXT_TICKET_ID), ownerName(nullptr), associatedEvent(Event::Event()), vipStatus(false) {}
 
 // CONSTRUCTOR WITH ARGUMENTS
-Ticket::Ticket(Event newAssociatedEvent, bool newVipStatus) : id(Ticket::NEXT_TICKET_ID) {
+Ticket::Ticket(const char* newOwnerName, Event newAssociatedEvent, bool newVipStatus) : id(Ticket::NEXT_TICKET_ID) {
 	this->setAssociatedEvent(newAssociatedEvent);
 	this->setVipStatus(newVipStatus);
 }
@@ -73,11 +77,32 @@ Ticket::Ticket(Event newAssociatedEvent, bool newVipStatus) : id(Ticket::NEXT_TI
 // COPY CONSTRUCTOR
 Ticket::Ticket(const Ticket& toBeCopied) {
 	this->id = toBeCopied.id;
+	this->ownerName = new char[strlen(toBeCopied.ownerName) + 1];
+	strcpy_s(this->ownerName, strlen(toBeCopied.ownerName) + 1, toBeCopied.ownerName);
 	this->associatedEvent = toBeCopied.associatedEvent;
 	this->vipStatus = toBeCopied.vipStatus;
 }
 
+// DESTRUCTOR
+Ticket::~Ticket() {
+	if (this->ownerName != nullptr) {
+		delete[] this->ownerName;
+	}
+}
+
 // SETTERS
+void Ticket::setOwnerName(const char* newOwnerName) {
+	if (strlen(newOwnerName) < Ticket::MIN_NAME_LENGTH || strlen(newOwnerName) > Ticket::MAX_NAME_LENGTH) {
+		throw std::exception("Invalid name length");
+	}
+	char* temp = new char[strlen(newOwnerName) + 1];
+	strcpy_s(temp, strlen(newOwnerName) + 1, newOwnerName);
+	if (this->ownerName != nullptr) {
+		delete[] this->ownerName;
+	}
+	this->ownerName = temp;
+}
+
 void Ticket::setAssociatedEvent(Event newAssociatedEvent) {
 	this->associatedEvent = newAssociatedEvent;
 }
@@ -91,6 +116,10 @@ int Ticket::getId() {
 	return this->id;
 }
 
+std::string Ticket::getOwnerName() {
+	return std::string(this->ownerName);
+}
+
 Event Ticket::getAssociatedEvent() {
 	return this->associatedEvent;
 }
@@ -101,12 +130,26 @@ bool Ticket::isVip() {
 
 // OPERATORS OVERLOADING
 void Ticket::operator=(const Ticket& toBeCopied) {
-	this -> id = toBeCopied.id;
+	if (this == &toBeCopied) {
+		return;
+	}
+	this->id = toBeCopied.id;
+	this->setOwnerName(toBeCopied.ownerName);
 	this->associatedEvent = toBeCopied.associatedEvent;
 	this->vipStatus = toBeCopied.vipStatus;
 }
 
+char Ticket::operator[](int index) {
+	return this->ownerName[index];
+}
+
 void operator>>(std::istream& console, Ticket& myTicket) {
+	std::cout << "Enter your name: ";
+	char buffer[Ticket::MAX_NAME_LENGTH + 1];
+	console.ignore();
+	console.getline(buffer, Ticket::MAX_NAME_LENGTH + 1);
+	myTicket.setOwnerName(buffer);
+
 	// associated event
 	Event::printEvents();
 	std::cout << "\nEnter ID of the event you want!\n";
@@ -131,6 +174,7 @@ void operator>>(std::istream& console, Ticket& myTicket) {
 
 void operator<<(std::ostream& console, const Ticket& myTicket) {
 	console << "\n================== TICKET #" << myTicket.id << " ==================";
+	console << "\nOwner Name: " << myTicket.ownerName;
 	console << "\nAssociated Event" << myTicket.associatedEvent;
 	console << "\nVIP Status: " << myTicket.vipStatus;
 	console << "\n===================================================\n";
--- ticketing-app-VS_PROJECT/ticketing-app-VS_PROJECT/Ticket.h
+++ ticketing-app-VS_PROJECT/ticketing-app-VS_PROJECT/Ticket.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "Event.h"
 #include <iostream>
+#include <string>
 
 class Ticket {
 	// NORMAL ATTRIBUTES
 	int id;
+	char* ownerName;
 	Event associatedEvent;
 	bool vipStatus;
 
@@ -14,8 +16,8 @@ class Ticket {
 
 	static int NEXT_TICKET_ID;
 
-	static char MIN_ROW;
-	static int MIN_SEAT;
+	static const int MIN_NAME_LENGTH;
+	static const int MAX_NAME_LENGTH;
 public:
 	// STATIC METHODS
 	static void printTickets();
@@ -25,20 +27,26 @@ public:
 
 	// CONSTRUCTORS
 	Ticket();
-	Ticket(Event newAssociatedEvent, bool newVipStatus);
+	Ticket(const char* newOwnerName, Event newAssociatedEvent, bool newVipStatus);
 	Ticket(const Ticket& toBeCopied);
 
+	// DESTRUCTOR
+	~Ticket();
+
 	// SETTERS
+	void setOwnerName(const char* newOwnerName);
 	void setAssociatedEvent(Event newAssociatedEvent);
 	void setVipStatus(bool newVipStatus);
 
 	// GETTERS
 	int getId();
+	std::string getOwnerName();
 	Event getAssociatedEvent();
 	bool isVip();
 	
 	// OPERATORS OVERLOADING
 	void operator=(const Ticket& toBeCopied);
+	char operator[](int index);
 
 	friend void operator>>(std::istream& console, Ticket& myTicket);
 	friend void operator<<(std::ostream& console, const Ticket& myTicket);
